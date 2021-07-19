e24bfc0216cd492cec07c24fa27e894a18bb1f24
Felipe Lapa do Nascimento <felipe.7lapa@hotmail.com>
2021-07-19 19:44:40 -0300

    Arquivo trocado

--- Python/CalculadorAreas.py
+++ Python/CalculadorAreas.py
@@ -1,15 +1,17 @@
 from time import sleep
-import area_func1 as af
+import ClassesCA as ca
+import FuncoesCA as fa
 
 print('Bem-vindx ao calculador de áreas')
 sleep(0.5)
 while True:
-    af.menu()
+    fa.Menu()
     sleep(1)
 
     try:
-        escolha = af.escolha_figura()
-        sleep(0.5)
+        escolha = int(input('Escolha: '))
+        if (escolha > 7) or (escolha < 0):
+            print('Digite novamente.')
 
     except (TypeError, SyntaxError, ValueError):
         print('Digite novamente.')
@@ -18,38 +20,61 @@ while True:
     else:
         # Triâgulo
         if escolha == 1:
-            areaT = af.triangulo()
-            af.func_Calculando(areaT)
+            base = float(input('Base:'))
+            altura = float(input('Altura:'))
+            triangulo = ca.FiguraPlana(base=base, altura=altura)
+            area = triangulo.Triangulo()
+            fa.Calculando(area)
 
-        # Quadrado
         if escolha == 2:
-            areaQ = af.quadrado()
-            af.func_Calculando(areaQ)
+            lado = float(input('Lado:'))
+            if lado <= 0:
+                print('Digite o lado novamente',
+                      'O lado de um quadrado não pode ser zero ou negativo.')
+            else:
+                quadrado = ca.FiguraPlana(base=lado)
+                area = quadrado.Quadrado()
+                fa.Calculando(area)
 
         # Círculo
         if escolha == 3:
-            af.circulo()
+            raio = float(input('Raio:'))
+            circulo = ca.FiguraPlana(raio=raio)
+            area = circulo.Circulo(pi=3.14)
+            fa.Calculando(area)
 
         # Losango
         if escolha == 4:
-            areaL = af.losango()
-            af.func_Calculando(areaL)
+            Dzao = float(input('Digite o valor da diagonal maior: '))
+            Dzinho = float(input('Digite o valor da diagonal menor: '))
+            trapezio = ca.FiguraPlana(Dzao=Dzao, Dzinho=Dzinho)
+            area = trapezio.Losango()
+            fa.Calculando(area)
 
         # Trapézio
         if escolha == 5:
-            areaTrap = af.trapezio()
-            af.func_Calculando(areaTrap)
+            Bzao = float(input('Digite o valor da base maior: '))
+            Bzinho = float(input('Digite o valor da base menor: '))
+            altura = float(input('Digite o valor da altura: '))
+            trapezio = ca.FiguraPlana(Bzao=Bzao, Bzinho=Bzinho, altura=altura)
+            area = trapezio.Trapezio()
+            fa.Calculando(area)
 
         # Cubo
         if escolha == 6:
-            areaCubo = af.cubo()
-            af.func_Calculando(areaCubo)
+            lado = float(input('Lado:'))
+            cubo = ca.FiguraEspacial(base=lado)
+            area = cubo.Cubo()
+            fa.Calculando(area)
 
         # Esfera
         if escolha == 7:
-            af.esfera()
+            raio = float(input('Raio:'))
+            esfera = ca.FiguraEspacial(raio=raio)
+            area = esfera.Esfera(pi=3.14)
+            fa.Calculando(area)
 
         # Sair
         if escolha == 0:
-            af.func_tchau()
+            fa.Tchau()
             break
